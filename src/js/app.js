0ad0f2bd037297fee7e484f3233a888f6cf1a42f
castillo <del.castillo.amn@gmail.com>
2019-04-30 17:56:26 +0100

    add wow classes to home and about

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,29 @@
 import "../sass/main.scss";
 import "jquery";
 import "slick-carousel";
+
 import "bootstrap";
 
 import { WOW } from "wowjs";
 import mixitup from "mixitup";
-// init wow js [webpack require that]
+
+// [webpack require this to implemet wow js]
 export default {
   init() {
     // JavaScript to be fired on all pages
     const wow = new WOW();
-
     wow.init();
   }
 };
+//main function
 $(function() {
   new WOW().init();
+  // the spinner loader will fade out after the window is fully loaded
+  $(window).on("load", function() {
+    $(".main-loader").fadeOut(500);
+  });
+
+  //slick js configuration
   $(".header__slider").slick({
     dots: false,
     autoplay: true,
@@ -33,18 +41,26 @@ $(function() {
 
     slidesToShow: 1
   });
-  $(window).on("load", function() {
-    $(".main-loader").fadeOut(500);
-  });
 
-  // //init mixitup js
+  // init mixitup
   var mixer = mixitup(".works-wrapper");
 
-  // //shifter
+  // auto placement active class between filter-btn elements
   $(".filter-btn").on("click", function() {
     $(this)
       .addClass("active")
       .siblings()
       .removeClass("active");
+  });
+
+  //nice scroll
+  $("body").niceScroll({
+    cursorcolor: "#2286b5", // change cursor color in hex
+    cursoropacitymin: 0.3, // change opacity when cursor is inactive (scrollabar "hidden" state), range from 1 to 0
+    cursoropacitymax: 1, // change opacity when cursor is active (scrollabar "visible" state), range from 1 to 0
+    cursorwidth: "5px", // cursor width in pixel (you can also write "5px")
+    cursorborder: "none", // css definition for cursor border
+    cursorborderradius: "3px", // border radius in pixel for cursor
+    zindex: "auto" | [number] // change z-index for scrollbar div
   });
 });
